f3ee0a33655333269bb1e0d9f790e5917135ec60
Kareem Ebrahem <90919499+mrx7014@users.noreply.github.com>
2023-06-30 18:58:08 +0300

    Add files via upload

---
+++ env_build_setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Coded By MRX7014
+
+sleep 1
+
+echo -e "\e[93mWelcome To Environment Build Setup\e[0m"
+
+sleep 2
+
+echo -e "\e[93mCoded By MRX7014\e[0m"
+
+sleep 2
+
+echo -e "\e[93mDon't Forget To Follow Me On Github\e[0m"
+
+sleep 1
+
+echo -e "\e[93mhttps://www.github.com/mrx7014\e[0m"
+
+sleep 2
+
+echo -e "\e[93mSetup Starting Now\e[0m"
+
+sleep 1
+
+sudo apt-get update -y && sudo apt-get install dialog bash sed wget git curl zip tar jq expect make cmake automake autoconf llvm lld lldb clang gcc binutils bison perl gperf gawk flex bc python3 python2 zstd openssl unzip cpio bc bison build-essential ccache liblz4-tool libsdl1.2-dev libstdc++6 libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zlib1g-dev libncurses5-dev bzip2 git gcc g++ libssl-dev gcc-aarch64-linux-gnu gcc-arm-linux-gnueabihf gcc-arm-linux-gnueabi dos2unix neofetch -y
+
+sudo apt-get update
+
+sudo apt-get remove openjdk-* icedtea-* icedtea6-* 
+
+sudo apt-get install git ccache automake lzop bison gperf build-essential zip curl zlib1g-dev g++-multilib python libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng maven
+
+sudo apt-get install bc bison build-essential ccache curl flex \
+g++-multilib gcc-multilib git-core gnupg gperf imagemagick lib32ncurses5-dev \
+lib32z-dev libc6-dev-i386 libgl1-mesa-dev libssl-dev libx11-dev \
+libxml2-utils unzip x11proto-core-dev xsltproc zip zlib1g-dev
+
+mkdir ~/bin && curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo && chmod a+x ~/bin/repo
+
+
+sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip
+
+sudo apt-get update && sudo apt-get install openjdk-8-jdk
+
+sudo apt install clang
+
+sudo apt install bc
+
+sleep 1
+
+echo -e "\e[93mThanks For Using This Script\e[0m"
